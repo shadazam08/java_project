4ed3007db17607dff5e93b27770abd2a04f4cabe
shadazam08 <azamshad08@gmail.com>
2022-08-30 20:44:39 +0530

    practice

---
+++ src/Star_Pattern/Left_Triangle_Star_Pattern.java
@@ -0,0 +1,17 @@
+package Star_Pattern;
+import java.util.*;
+public class Left_Triangle_Star_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = 2*(n-i); j >= 0; j--){
+                System.out.print(" ");
+            }
+            for(int k = 0; k <= i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Star_Pattern/star_pattern1.java
@@ -0,0 +1,17 @@
+package Star_Pattern;
+import java.util.*;
+public class star_pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = n - i; j >= 0; j--){
+                System.out.print(" ");
+            }
+            for(int k = 0; k <= i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
