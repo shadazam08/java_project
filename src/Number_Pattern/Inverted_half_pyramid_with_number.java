d3035ece95af3531748e282e050f5cf986ba6b13
shadazam08 <azamshad08@gmail.com>
2022-09-03 12:25:08 +0530

    practice

--- src/Number_Pattern/Inverted_half_pyramid_with_number.java
+++ src/Number_Pattern/Inverted_half_pyramid_with_number.java
@@ -5,9 +5,9 @@ public class Inverted_half_pyramid_with_number {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // outer loop
-        for(int i = n; i >= 1; i--){
+        for(int i = 1; i <= n; i++){
             // inner loop
-            for(int j = 1; j <= i; j++){
+            for(int j = 1; j <= n-i+1; j++){
                 System.out.print(j + " ");
             }
             System.out.println();
