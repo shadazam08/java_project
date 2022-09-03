e79cb0f67ea1469fe00ab8d317254d3af5ece40a
shadazam08 <azamshad08@gmail.com>
2022-09-03 10:50:14 +0530

    practice

--- src/Star_Pattern/Solid_Rectangle.java
+++ src/Star_Pattern/Solid_Rectangle.java
@@ -1,14 +1,11 @@
 package Star_Pattern;
 import java.util.*;
-public class Left_Triangle_Star_Pattern {
+public class Solid_Rectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 0; i < n; i++){
-            for(int j = 2*(n-i); j >= 0; j--){
-                System.out.print(" ");
-            }
-            for(int k = 0; k <= i; k++){
+            for(int j = 0; j < n; j++){
                 System.out.print("* ");
             }
             System.out.println();
