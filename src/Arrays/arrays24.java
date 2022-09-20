36dc92ff7a01e590286809fe9c2e59b78a067e65
shadazam08 <azamshad08@gmail.com>
2022-09-21 00:04:17 +0530

    practice

--- src/Arrays/arrays24.java
+++ src/Arrays/arrays24.java
@@ -11,17 +11,19 @@ public class arrays24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int count=0;
         int[] arr = new int[n];
         for(int i=0; i<=n-1; i++){
             arr[i] = sc.nextInt();
         }
+        Arrays.sort(arr);
         for(int i=0; i<=n-1; i++){
-            for(int j=i+1; j< n; j++){
-                if(arr[i] == arr[j]){
 
-                    System.out.println(arr[i]+ " "+ arr[j]);
+                if(arr[i] == arr[i+1]){
+                    count++;
+                    System.out.println(arr[i]+ " "+ count);
                 }
-            }
+
         }
     }
 }
