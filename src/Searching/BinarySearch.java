b47a3fd0a4464297852c575821e2c0d8d8c6c7f9
shadazam08 <azamshad08@gmail.com>
2022-10-02 20:52:43 +0530

    practice

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -1,5 +1,5 @@
 package Searching;
-
+import java.util.Scanner;
 public class BinarySearch {
     static int binarySearch(int[] arr, int x){
         int low = 0, high = arr.length-1;
@@ -17,11 +17,15 @@ public class BinarySearch {
         return -1;
     }
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 //        BinarySearch ob = new BinarySearch();
-        int[] arr = { 2, 3, 4, 10, 40 };
+        int[] arr = new int[n];
 //        int n = arr.length;
-        int x = 10;
-
+        int x = sc.nextInt();
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
         int result = binarySearch(arr, x);
         if (result == -1)
             System.out.println("Element not present");
